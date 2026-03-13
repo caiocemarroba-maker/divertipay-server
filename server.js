c4d00f86e9d669037f530d58802a338b40070fc1
caiocemarroba-maker <caiocemarroba@gmail.com>
2026-03-13 17:16:33 -0400

    Update server.js

--- server.js
+++ server.js
@@ -579,4 +579,4 @@ setInterval(async () => {
 }, 60000);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('DivertiPay rodando na porta ' + PORT));
+app.listen(PORT, () => console.log('DivertiPay rodando na porta ' + PORT)); 
